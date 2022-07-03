43298dd94532c0296a7c204947f7e342f46453e3
AttractiveMinki <70371239+AttractiveMinki@users.noreply.github.com>
2022-07-03 14:49:44 +0900

    add print result

--- sample_codes/sort_by_lambda.py
+++ sample_codes/sort_by_lambda.py
@@ -1,8 +1,10 @@
 def solution(numbers):
     numbers.sort(key=lambda x: (x[1], x[0]))
     print(numbers)
+    ## [[1, 0], [7, 0], [1, 1], [2, 2], [3, 2], [2, 3], [0, 4], [5, 6], [7, 7]]
     numbers.sort(key=lambda x: x[0])
     print(numbers)
+    ## [[0, 4], [1, 0], [1, 1], [2, 2], [2, 3], [3, 2], [5, 6], [7, 0], [7, 7]]
     return
     
 
--- sample_codes/string_num_convert.py
+++ sample_codes/string_num_convert.py
@@ -2,13 +2,13 @@
 word = 'A'
 word2 = 'a'
 # 대문자
-print(ord(word) - 65)
+print(ord(word) - 65) ## 0
 # 소문자
-print(ord(word2) - 97)
+print(ord(word2) - 97) ## 0
 
 # num -> str
 num = 0
 # 대문자
-print(chr(num + 65))
+print(chr(num + 65)) ## A
 # 소문자
-print(chr(num + 97))
+print(chr(num + 97)) ## a
