0541329da47cbad97d3f2088336d4d48056273b9
AttractiveMinki <70371239+AttractiveMinki@users.noreply.github.com>
2022-09-28 13:41:56 +0900

    add dictionary sort by lambda

--- sample_codes/sort_by_lambda.py
+++ sample_codes/sort_by_lambda.py
@@ -9,3 +9,13 @@ def solution(numbers):
     
 
 solution([[1, 0], [2, 3], [3, 2], [0, 4], [5, 6], [7, 0], [1, 1], [2, 2], [7, 7]])
+
+
+### dictionary 정렬하기 ###
+# dictionary 정렬시 sorted 안에서 items() 사용하면 쉽게 정렬 가능.
+
+my_dict = {1: 3, 2: 5, 3: 1, 4: 3}
+print(my_dict) # {1: 3, 2: 5, 3: 1, 4: 3}
+
+my_dict = sorted(my_dict.items(), key=lambda x: (x[1], x[0]))
+print(my_dict) # [(3, 1), (1, 3), (4, 3), (2, 5)]
